84cdce91a5ebba55b67fe1fcbab0016bf8b7ec1d
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-07-13 04:54:33 +0300

    Added test for adding a contact

---
+++ test_add_contact.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import unittest
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+    
+    def test_add_contact(self):
+        driver = self.driver
+        driver.get("https://localhost/addressbook/")
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+        driver.find_element_by_link_text("add new").click()
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys("John")
+        driver.find_element_by_name("middlename").click()
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys("Robert")
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys("Smith")
+        driver.find_element_by_name("nickname").click()
+        driver.find_element_by_name("nickname").clear()
+        driver.find_element_by_name("nickname").send_keys("Bob")
+        driver.find_element_by_name("title").click()
+        driver.find_element_by_name("title").clear()
+        driver.find_element_by_name("title").send_keys("Sales manager")
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("company").send_keys("Some company")
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").clear()
+        driver.find_element_by_name("address").send_keys("New York, 45th Street, 57")
+        driver.find_element_by_name("home").click()
+        driver.find_element_by_name("home").clear()
+        driver.find_element_by_name("home").send_keys("56789450")
+        driver.find_element_by_name("mobile").click()
+        driver.find_element_by_name("mobile").clear()
+        driver.find_element_by_name("mobile").send_keys("37890406")
+        driver.find_element_by_name("work").click()
+        driver.find_element_by_name("work").clear()
+        driver.find_element_by_name("work").send_keys("47890593")
+        driver.find_element_by_name("fax").click()
+        driver.find_element_by_name("fax").clear()
+        driver.find_element_by_name("fax").send_keys("47589341")
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("jsmith@smth.com")
+        driver.find_element_by_name("bday").click()
+        Select(driver.find_element_by_name("bday")).select_by_visible_text("14")
+        driver.find_element_by_xpath("//option[@value='14']").click()
+        driver.find_element_by_name("bmonth").click()
+        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("November")
+        driver.find_element_by_xpath("//option[@value='November']").click()
+        driver.find_element_by_name("byear").click()
+        driver.find_element_by_name("byear").clear()
+        driver.find_element_by_name("byear").send_keys("1993")
+        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        driver.find_element_by_link_text("home page").click()
+        driver.find_element_by_link_text("Logout").click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
