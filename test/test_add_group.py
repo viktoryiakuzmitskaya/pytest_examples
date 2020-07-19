5c29cb88ae2d3643b5f753c49832ef38502609eb
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-07-20 01:55:56 +0300

    Added packages for different responsibility areas (test for group creation only)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application_group import ApplicationGroup
+from model.group import Group
+from fixture.application_group import ApplicationGroup
 
 
 @pytest.fixture
