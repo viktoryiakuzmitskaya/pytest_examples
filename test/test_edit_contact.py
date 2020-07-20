9f84fefe1da19ddf67aa136e4312f813980b3b99
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-07-20 16:08:02 +0300

    Added test for a first contact modification

---
+++ test/test_edit_contact.py
@@ -0,0 +1,9 @@
+from model.contact import Contact
+
+
+def test_edit_first_contact(app):
+    app.session.login("admin", "secret")
+    app.contact.edit_first_contact(Contact(firstname="James", middlename="Christian", lastname="Levis", nickname="Chris", title="Regional manager", company="Some company 2",
+                                   address="New York, 47th Street, 57", home="56789451", mobile="37890407", work="47890597", fax="47589342",
+                                   email="jlevis@smth.com", bday="15", bmonth="October", byear="1992"))
+    app.session.logout()
