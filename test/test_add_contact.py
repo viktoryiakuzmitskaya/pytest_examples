c42166c09cc3b1579466bb386ce6fc519c934a25
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-07-22 01:18:33 +0300

    Extracted a method for filling out contact form

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,6 +6,6 @@ def test_add_contact(app):
     app.session.login("admin", "secret")
     app.contact.create(Contact(firstname="John", middlename="Robert", lastname="Smith", nickname="Bob", title="Sales manager", company="Some company",
                                    address="New York, 45th Street, 57", home="56789450", mobile="37890406", work="47890593", fax="47589341",
-                                   email="jsmith@smth.com", bday="14", bmonth="November", byear="1993"))
+                                   email="johnsmith@smth.com", bday="14", bmonth="November", byear="1993"))
     app.session.logout()
 
