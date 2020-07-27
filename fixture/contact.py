3adb8c073ec6c131a3b7faa18774491faf86a585
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-07-27 03:01:55 +0300

    Added preconditions checking for deletion and modification of contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -73,3 +73,8 @@ class ContactHelper:
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.is_on_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,4 +2,6 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="Michael", lastname="Scott", mobile="1234567", email="mscott@test.com"))
     app.contact.edit_first_contact(Contact(firstname="James", bday="15", bmonth="October", byear="1992"))
