fe1e3b70e31ecbf4c85e7c990fe2f9ca6f7dc244
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-03 02:06:49 +0300

    Added caching and hashing for groups

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -8,7 +8,7 @@ def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="TestName"))
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
