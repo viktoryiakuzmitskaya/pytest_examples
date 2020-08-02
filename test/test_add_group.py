b2e560588543c5ce509f87e819c8e03bc5e9dd89
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-02 15:55:43 +0300

    Added loading group information into lists, checking lists length for add/delete/edit group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,5 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group_name", header="group_header", footer="group_footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,6 +2,9 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="TestName"))
     app.group.edit_first_group(Group(name="group_name_edited"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
