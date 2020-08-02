e3181d95d6c235ab50205b3043af30dab4283502
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-02 16:39:42 +0300

    Added sorting lists before comparison - for groups only

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,10 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="group_name", header="group_header", footer="group_footer"))
+    group = Group(name="group_name", header="group_header", footer="group_footer")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,8 +3,12 @@ from model.group import Group
 
 def test_edit_first_group(app):
     old_groups = app.group.get_group_list()
+    group = Group(name="group_name_edited")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name="TestName"))
-    app.group.edit_first_group(Group(name="group_name_edited"))
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
