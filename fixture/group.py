f9f59d37525d6c3962eb09bac4b6741ad9cd21bf
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-03 03:27:02 +0300

    Added random deletion and modification for groups

--- fixture/group.py
+++ fixture/group.py
@@ -33,21 +33,31 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
-    def edit_first_group(self, new_group_data):
+    def edit_first_group(self):
+        self.edit_group_by_index(0)
+
+    def edit_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_group_by_index(index)
         # edit group
         wd.find_element_by_name("edit").click()
         self.fill_out_form(new_group_data)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,14 +1,16 @@
 from model.group import Group
+from random import randrange
 
 
-def test_edit_first_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="group_name_edited")
-    group.id = old_groups[0].id
+def test_edit_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="TestName"))
-    app.group.edit_first_group(group)
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group = Group(name="group_name_edited")
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(index, group)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
