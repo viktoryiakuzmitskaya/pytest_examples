f2727f4303428df4525219089271ceeda85d038b
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-14 01:34:53 +0300

    Added parametrized tests for a group creation

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_group(app):
+#testdata = [Group(name="", header="", footer="")] + [
+    #Group(name=random_string("name_", 10), header=random_string("header_", 20), footer=random_string("footer_", 20))
+    #for i in range(5)
+#]
+
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name_", 10)]
+    for header in ["", random_string("header_", 20)]
+    for footer in ["", random_string("footer_", 20)]
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="group_name", header="group_header", footer="group_footer")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
