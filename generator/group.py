a6aa0f46835a88a78a569936c31fbd1e45123430
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-22 22:56:14 +0300

    Modified tests for adding and deleting a group - group list now is retrieved from the database

--- generator/group.py
+++ generator/group.py
@@ -24,7 +24,8 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + " "*10
+    #symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [Group(name="", header="", footer="")] + [
