bed6f8c9c3fed955d31ad131a4700468e6708b12
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-23 16:44:43 +0300

    Added conditions of selection of loaded data

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -19,6 +20,18 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 db_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return db_list
+
+    def get_contact_list(self):
+        db_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                db_list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
         finally:
             cursor.close()
         return db_list
