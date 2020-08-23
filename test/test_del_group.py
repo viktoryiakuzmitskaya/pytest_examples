f20a446696a11271fc8a38e358d379b5fec35bbd
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-23 12:32:13 +0300

    Added removable ui check

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 import random
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="TestName"))
     old_groups = db.get_group_list()
@@ -11,3 +11,6 @@ def test_delete_some_group(app, db):
     assert len(old_groups) - 1 == app.group.count()
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
