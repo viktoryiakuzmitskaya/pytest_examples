757586361421938901be338cac95ad5bb3ed60dd
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-08-24 23:53:08 +0300

    Modified test to create, edit, delete group so the group list is loaded from the database

--- fixture/group.py
+++ fixture/group.py
@@ -80,6 +80,19 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def edit_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        self.select_group_by_id(id)
+        # edit group
+        wd.find_element_by_name("edit").click()
+        self.fill_out_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,17 @@
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_edit_group(app):
+def test_edit_group(app, db, check_ui):
     if app.group.count() == 0:
         app.group.create(Group(name="TestName"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="group_name_edited")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(index, group)
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    new_group_data = Group(name="group_name_modified")
+    app.group.edit_group_by_id(group.id, new_group_data)
+    new_groups = db.get_group_list()
     assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    group.name = new_group_data.name
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
