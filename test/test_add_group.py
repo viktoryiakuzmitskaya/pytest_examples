fd3bc234f388ebc19851cd162cae8c7b82939259
Viktoryia Kuzmitskaya <victoriakuzmitskaya@gmail.com>
2020-09-12 18:37:28 +0300

    Added gherkin scenarios for creating and deleting a group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,7 +8,7 @@ def test_add_group(app, db, json_groups, check_ui):
     app.group.create(group)
     new_groups = db.get_group_list()
     old_groups.append(group)
-    assert old_groups == new_groups
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
